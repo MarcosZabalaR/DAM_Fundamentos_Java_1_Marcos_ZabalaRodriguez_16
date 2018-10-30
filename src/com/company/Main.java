5fb9343ca7b67df3bd07cfdaca74171705df0ecf
Marcos <marcos.zabala@escuelaestech.es>
2018-10-30 19:50:39 +0100

    Ejercicio 16 sin terminar

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido, por favor introduzca una fecha a corroborar.");
+        Scanner sc = new Scanner(System.in);
+        int day=sc.nextInt();
+        int month=sc.nextInt();
+        int year=sc.nextInt();
+        check(day,month,year);
+    }
+
+    /*
+    Función main donde pediremos las variables día, mes y año al usuario.
+     */
+
+    private static void check(int day, int month, int year) {
+        if(year<0){
+            System.out.println("El año es incorrecto.");
+        }else{
+            if((month==2 && day>28||day<0)||(month==4 && day<0 || day>30)||(month==6 && day<0 || day>30)||(month==9 && day<0 || day>30)||(month==10 && day<0 || day>30)){
+                System.out.println("El día es incorrecto.");
+            }else{
+                if(month<0||month>12){
+                    System.out.println("El mes es incorrecto.");
+                }else{
+                    System.out.println("La fecha es correcta.");
+                }
+            }
+        }
+    }
+
+    /*
+    Aquí comprobaremos que las variables son correctas.
+     */
+}
